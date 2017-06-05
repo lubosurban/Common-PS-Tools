39355fc29e987376d6b20932e017ab2abf91cf97
Lubos Urban <lubos_urban@tempest.sk>
2017-06-05 14:41:43 +0200

    Renamed FSExctensions + added new function

--- MiscTools.ps1
+++ MiscTools.ps1
@@ -1,6 +1,6 @@
 # =============================================
 # =============================================
-# Script name: FSExtensions.ps1
+# Script name: MiscTools.ps1
 # =============================================
 # =============================================
 
@@ -28,12 +28,12 @@ function Clear-DirectorySubTree
             $preserveDir = $false
 
             $i = 0
-            while((!$preserveDir) -and ($i -lt $excludedDirectories.Length)) 
+            while((!$preserveDir) -and ($i -lt $excludedDirectories.Length))
             {
-                $preserveDir = $item.FullName -like $excludedDirectories[$i] 
-                $i++ 
+                $preserveDir = $item.FullName -like $excludedDirectories[$i]
+                $i++
             }
-            
+
             if($preserveDir)
             {
                 $allItemsRemoved = $false
@@ -48,10 +48,10 @@ function Clear-DirectorySubTree
             $preserveFile = $false
 
             $i = 0
-            while((!$preserveFile) -and ($i -lt $excludedFiles.Length)) 
+            while((!$preserveFile) -and ($i -lt $excludedFiles.Length))
             {
-                $preserveFile = $item.FullName -like $excludedFiles[$i] 
-                $i++ 
+                $preserveFile = $item.FullName -like $excludedFiles[$i]
+                $i++
             }
 
             if($preserveFile)
@@ -71,4 +71,41 @@ function Clear-DirectorySubTree
     }
 
     return($allItemsRemoved)
+}
+
+# ---------------------------------------------
+# Function: Start-ProcessAndValidateExitCode
+# ---------------------------------------------
+function Start-ProcessAndValidateExitCode
+{
+  Param
+  (
+    [parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [String]
+    $executable,
+    [String]
+    $parameters
+  )
+
+  try
+  {
+    if($parameters)
+    {
+      $process = Start-Process -Wait -FilePath $executable -ArgumentList $parameters -PassThru
+    }
+    else
+    {
+      $process = Start-Process -Wait -FilePath $executable -PassThru
+    }
+  }
+  catch
+  {
+    throw("Unable to execute `"$executable $parameters`", executable file probably does not exists or it is not valid executable file.")
+  }
+
+  if($process.ExitCode -ne 0)
+  {
+    throw("Process (`"$executable $parameters`") execution terminated with exit code: " + $process.ExitCode)
+  }
 }
